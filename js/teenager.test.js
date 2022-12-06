2bc9094d280d8f0586f673c1163aa69268979478
Mengzhen <liaomengzhen@gmail.com>
2022-12-06 23:32:52 +0100

    change teenager.test.js and package.json

--- js/teenager.test.js
+++ js/teenager.test.js
@@ -1,6 +1,15 @@
 import { checkTeenagerStatus } from "./teenager.js";
+import { test } from "@jest/globals";;
 
-test("returns correct teenager status", () => {
-  const teenagerStatus = checkTeenagerStatus(15);
-  expect(teenagerStatus).toBe(true);
+const testData = [
+  [12, false],
+  [13, true],
+  [19, true],
+  [20, false],
+  [70, false],
+];
+
+test.each(testData)("returns correct teenager status", (age, expectedValue) => {
+  const teenagerStatus = checkTeenagerStatus(age);
+  expect(teenagerStatus).toBe(expectedValue);
 });
